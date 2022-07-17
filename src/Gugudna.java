6083cc2f353c3b670a958145ad20df31eff37073
jinchangkyu <wlsalsrb2@gmail.com>
2022-07-17 14:44:16 +0900

    배열을 이용한 구구단

--- src/Gugudna.java
+++ src/Gugudna.java
@@ -1,13 +1,25 @@
 
 import java.util.Scanner;
 public class Gugudna {
-    public static void main(String[] args) {
+    public static int[] calculate(int times) {
         int[] result = new int[9];
         for (int i = 0; i < result.length; i++) {
-            result[i] = 2 * (i + 1);
+            result[i] = times * (i + 1);
         }
-        for(int i = 0; i < result.length; i++) {
+        return result;
+
+    }
+    public static void print(int[] result) {
+        for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
     }
+
+    public static void main(String[] args) {
+        for(int i =2; i < 10; i++) {
+            int[] result = calculate(i);
+            print(result);
+        }
+    }
 }
+
