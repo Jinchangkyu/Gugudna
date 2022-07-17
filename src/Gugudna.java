148fbf54ff3b91f56e178ae6c91c10cc2a23ae41
jinchangkyu <wlsalsrb2@gmail.com>
2022-07-17 14:56:15 +0900

    구구단

--- src/Gugudna.java
+++ src/Gugudna.java
@@ -1,5 +1,4 @@
 
-import java.util.Scanner;
 public class Gugudna {
     public static int[] calculate(int times) {
         int[] result = new int[9];
@@ -12,13 +11,6 @@ public class Gugudna {
     public static void print(int[] result) {
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
-        }
-    }
-
-    public static void main(String[] args) {
-        for(int i =2; i < 10; i++) {
-            int[] result = calculate(i);
-            print(result);
         }
     }
 }
