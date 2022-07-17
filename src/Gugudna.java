ce6213e054a3765cb922c861a68ff51bee9e7986
jinchangkyu <wlsalsrb2@gmail.com>
2022-07-17 13:03:11 +0900

    구구단

--- src/Gugudna.java
+++ src/Gugudna.java
@@ -1,18 +1,21 @@
-public class Gugudna {
-    public static void main(String[] args) {
+
+import java.util.Scanner;
+    public class Gugudna {
+        public static void main(String[] args) {
         // 2단
-        System.out.println("2단");
-        System.out.println(2 * 1);
-        System.out.println(2 * 2);
-        System.out.println(2 * 3);
-        System.out.println(2 * 4);
-        System.out.println(2 * 5);
-        System.out.println(2 * 6);
-        System.out.println(2 * 7);
-        System.out.println(2 * 8);
-        System.out.println(2 * 9);
+//        System.out.println("2단");
+//        System.out.println(2 * 1);
+//        System.out.println(2 * 2);
+//        System.out.println(2 * 3);
+//        System.out.println(2 * 4);
+//        System.out.println(2 * 5);
+//        System.out.println(2 * 6);
+//        System.out.println(2 * 7);
+//        System.out.println(2 * 8);
+//        System.out.println(2 * 9);
 
         // 3단
+        /*
         System.out.println("3단");
         System.out.println(3 * 1);
         System.out.println(3 * 2);
@@ -23,6 +26,42 @@ public class Gugudna {
         System.out.println(3 * 7);
         System.out.println(3 * 8);
         System.out.println(3 * 9);
+        */
 
+        // 4단
+        /*
+        System.out.println("4단");
+        int reuslt = 4 * 1;
+        System.out.println(reuslt);
+        reuslt = 4 * 2;
+        System.out.println(reuslt);
+        reuslt = 4 * 3;
+        System.out.println(reuslt);
+        reuslt = 4 * 4;
+        System.out.println(reuslt);
+        reuslt = 4 * 5;
+        System.out.println(reuslt);
+        reuslt = 4 * 6;
+        System.out.println(reuslt);
+        reuslt = 4 * 7;
+        System.out.println(reuslt);
+        reuslt = 4 * 8;
+        System.out.println(reuslt);
+        reuslt = 4 * 9;
+        System.out.println(reuslt);
+        */
+
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number * 1);
+        System.out.println(number * 2);
+        System.out.println(number * 3);
+        System.out.println(number * 4);
+        System.out.println(number * 5);
+        System.out.println(number * 6);
+        System.out.println(number * 7);
+        System.out.println(number * 8);
+        System.out.println(number * 9);
     }
 }
